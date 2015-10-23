35b7fc0a66c8d97474cf3791c33a663f10da6a9c
steelbrain <aneesiqbalbhatti@gmail.com>
2015-10-23 01:38:29 -0600

    :new: Add different build targets

---
+++ src/browser.js
@@ -0,0 +1,5 @@
+// @Compiler-Browserify "true"
+// @Compiler-Transpile "true"
+// @Compiler-Minify "true"
+// @Compiler-Output "../dist/x-notification.browser.min.js"
+window.XNotification = require('../')
